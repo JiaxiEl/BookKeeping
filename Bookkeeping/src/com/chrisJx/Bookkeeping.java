70ec78717546404691cd3082afcffb3b20a1d3b6
Jiaxi Chen <jiaxiechris@gmail.com>
2021-02-11 17:48:41 -0800

    test

--- Bookkeeping/src/com/chrisJx/Bookkeeping.java
+++ Bookkeeping/src/com/chrisJx/Bookkeeping.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 public class Bookkeeping {
     public static void main(String [] args){
         Bookkeeping bookkeeping = new Bookkeeping();
-
+        PersonalAcccount personalAcccount = new PersonalAcccount();
 
     }
     public void getData(){
